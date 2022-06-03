92f8dfa6773e7957abf4a25e9fb672f04e448ae4
Aldi Filopati <101870918+afilo@users.noreply.github.com>
2022-06-03 13:41:14 +0200

    Delete db_configure.py

--- src/db_configure.py
+++
@@ -1,8 +0,0 @@
-import json
-
-with open('config.json', 'r') as f:
-    config = json.load(f)
-
-host = config['host'] # host connection url
-name = config['name'] # name of the database to access
-authMechanism = config['authMechanism'] # authentication mechanism
